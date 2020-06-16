48319f6ab70cf387390075b458539331ae615aa9
byambaa <byambaa0918@gmail.com>
2020-06-16 10:03:12 +0800

    my first commit

---
+++ src/components/toolbar/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import css  from "./style.module.css";
+const Toolbar=()=>{
+    return <div className={css.Toolbar}>toolbar</div>
+}
+export default Toolbar;
---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Toolbar from "../components/toolbar";
+class App extends Component{
+  render(){
+  return (
+    <div>
+      <Toolbar/>
+    </div>
+  );
+  }
+}
+
+export default App;
